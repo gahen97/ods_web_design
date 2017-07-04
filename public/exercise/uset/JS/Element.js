e2b2a2cb4feaf08593283edd21b24f0f5c36043f
seanhodges61 <seanhodges@cmail.carleton.ca>
2017-07-04 16:37:07 -0400

    getting touch devices working

--- public/exercise/uset/JS/Element.js
+++ public/exercise/uset/JS/Element.js
@@ -1,92 +1,53 @@
-/*jshint esversion: 6 */ 'use strict';
-
 /*
-  Element should have:
-    - Making a new DOM Element to represent the element
-    - Way to store value, get value, set value
-    - Event handling:
-      - dragstart
-      - dragstop
-      - click
+  Everything for Elements that'd likely change between exercises.
+
+  I've got the devil on my shoulder. Over and over.
+  And I just can't sink any lower. Lower and lower.
+  The hounds of hell are getting closer. Closer and closer.
+  I've got the devil on my shoulder. Over and over.
+  Oh!
+
+  Follow that rainbow, my lucky old man.
+  There ain't no pot of gold - just kappa tokens.
+  I found the key to life - the lock was broken.
+  All my accomplishments are best left unspoken!
+
+  I've dug a hole so deep, I'm gonna drown in my mistakes.
+  Can't even sell my soul 'cause it ain't worth shit to take.
 */
 
-class Element {
-  static nextId () {
-    return Element.currentId ++;
+class Element extends ElementBase {
+  constructor(){
+    super (...arguments);
   }
 
-  constructor (value) {
-    this.value = value;
-    this.id = Element.nextId ();
+  draw () {
+    // TODO HARDCODING IS BAD. MAYBE MOVE SELECTORS TO DEFS?
+    var elementDiv = $(ELEMENT_TEMPLATE).clone ();
+    var model      = $(MODEL_DISPLAY);
+    var span       = $("span", elementDiv);
 
-    this.graphics = new ElemGraphics (this);
-    this.element = this.graphics.draw ();    //change name to generate??
-    this.DomEvents = this.addEvents (); //will be moved into view
+    // set the text ...
+    span.text (this.value).data ("id", this.id); // TODO There's gotta be a better way to do this
 
+    // parent it to the main div, add the stuff, return
+    elementDiv.insertAfter (model).data ("id", this.id);
+
+    this.addControls (elementDiv);
+
+    return elementDiv;
   }
 
-  getElementDiv () { return this.element; }
 
-  addEvents () {
-    // TODO This is called when control is being constructed,
-    // which ends up throwing an error ...
-    // and means nothing works. so, fix this. And find a better way than this, eh?
-    /*setTimeout(() => {
-      // NOTE : accessing global 'control' object is bad TODO
-      var event = control.getDomEventHandler (ELEM_EVENTS_ID);
-      if (!event) {
-        console.error ("Could not find Element events handler .... blowing up");
-        return -1;
-      }
-
-      event.push (this.element);
-      return 0;
-    }, 10);*/
-  }
-
-  // add draggable ...
-  addControls (e) { //make draggable
-    if (!e) e = this.element;
-    $ (e).draggable ({
-      containment: "parent"
-    });
-  }
-
-  // set active ... adds/removes class
   setActive (isActive) {
-    var element = $(this.element);
-    if (isActive)
-      element.addClass ("active");
-    else
-      element.removeClass ("active");
-
+      var element = $(this.element);
+      if (isActive)
+        element.addClass ("active");
+      else
+        element.removeClass ("active");
   }
 
-  // Draw the element into the DOM
-  draw () {     //change name to generate?? also need to redo
-    this.graphics.draw ();
-  }
-
-  moveTo (os) {
-    $ (this.element).offset (os);
-  }
-
-  // Remove the element from the DOM
-  remove () {
-    $(this.element).remove ();
-  }
-
-  // getters
-  getId () { return this.id; }
-  getValue () { return this.value; }
-
-  // note: 'true' value is basically an object version. parses null to null ; empty to undefined; else to itself
-  getTrueValue () {
-    var val = this.getValue ();
-    if (!val) return;
-    if (val === NULL_CHARACTER) return null;
-    return val;
+  moveTo (offset) {
+    $ (this.element).offset (offset);
   }
 }
-
-Element.currentId = 1000;
--- public/exercise/uset/JS/View.js
+++ public/exercise/uset/JS/View.js
@@ -1,158 +1,43 @@
+/*
+  Mainly for parts of the View that'll change between exercises,
+  so this can be stored outside of the main View stuff
 
-/*jshint esversion: 6 */ 'use strict';
-/* Hey I'm back */
+  cry, agony.
+  cry, agony.
+*/
 
-class View {
-  constructor() {
-    this.eventHandlers = [ ];
-    this.eventsById    = { };
-      // this maps DomEventHandlers by ID : Handler. This is only added to when
-      // id is specified.
-
-    this.elements = { };
-    this.elementsByValue = { };
-
-    this.modelDivHelper = new Div ($ (MODEL_MAIN)); // #TODO
-    this.graphics       = new Graphics (this);
+class View extends ViewBase {
+  constructor(){
+    super(...arguments);
   }
 
-  /* ---- START - ADD THE NULL ELEMENT ---- */
-  start()
-  {
-    this.addElement (NULL_CHARACTER, {withinModel: true});
-  }
-
-  /* ---- CLEAR THE MODEL ---- */
-  clear ()
-  {
-    for (var key in this.elements)
-      if (this.elements [key].getValue() !== NULL_CHARACTER)
-        this.removeElementById (key);
-  }
-  /* ---- EVENT HANDLERS ---- */
-  register (eh)
-  {
-    DOMEventHandler.registerEventHandler (eh);
-  }
-
-  addDOMEvent (event) {
-    var triggerMap = { };
-
-    var currData = null;
-
-    for (var evt in event.evtsArr)
-    {
-      currData = event.evtsArr[evt];
-      triggerMap[currData.domEvtName] = currData.customEvtName;
-    }
-
-    var newHandler = new DOMEventHandler (event.elem, triggerMap);
-    this.eventHandlers.push (newHandler);
-
-    // Handlers by ID
-    if (event.id)
-      this.eventsById[event.id] = newHandler;
-  }
-
-  getEventHandler (id) {
-    return this.eventsById [id];
-  }
-
-  /* ---- ELEMENTS ----- */
-
-  // add a new element
-  addElement (value, options) {
-    if (!options) options={};
-
-    // add the element & push it into the elements object
-    var newElement                                = new Element (value);
-    this.elements [newElement.getId ()]           = newElement;
-    this.elementsByValue [newElement.getValue ()] = newElement;
-
-    if (options.withinModel)
-      this.drawWithinModel (newElement);
-
-    if (options.events !== false) {
-      // Moved this into here. Now Element doesn't need to access Control, which it shouldn't
-      var e = this.getEventHandler (ELEM_EVENTS_ID);
-      if (e) e.push (newElement.getElementDiv ());
-    }
-  }
-
-  // remove an element
-  removeElementById (id) {
-    var element = this.elements [id];
-    if (!element) return false;
-
-    delete this.elementsByValue [element.getValue()];
-    delete this.elements[id];
-
-    // Remove the element from our event handler - if we remove it, it shouldn't be connected
-    //   to our event handling
-    var e = this.getEventHandler (ELEM_EVENTS_ID);
-    if (e) e.remove (element.getElementDiv ());
-
-    element.remove ();
-
-    return element;
-  }
-
-  removeElement (elem) {
-    var id = $(elem).data ("id");
-    return this.removeElementById (id);
-  }
-
-  // get an element
-  getElementById (id) {
-    return this.elements [id];
-  }
-
-  getElement (e) {
-    var id = $(e).data ("id");
-    return this.getElementById (id);
-  }
-
-  getValueFromElementDiv (e) {
-    var elem = this.getElement (e);
-    return elem && elem.getValue ();
-  }
-
-  // find an element
-  findByValue (value)
-  {
-    return this.elementsByValue [value];
-  }
-
-  // draw an element within the model
-  drawWithinModel (element) {
-    var pos = this.modelDivHelper.randomPosition ();
-    element.moveTo (pos);
-  }
-
-  // is element over top of the model?
-  isElementOverModel (element) {
-    // NTS: Element here is the div
-    return this.modelDivHelper.elementOver (element);
-  }
-
-  // set active element
-  setActive (element) {
-    if (this.activeElement)
-      this.activeElement.setActive (false);
-
-    this.activeElement = element;
-    if (element) element.setActive (true);
-  }
-
-
-  /* ---- MODELS ------ */
   displayModel (m) {
-    this.graphics.drawModel (m);
-  }
+    // TODO Remove any elements that should not be shown
+    for (var k in this.elements){
+      var value = this.elements [k].getValue ();
+      var elem  = this.elements [k].getElementDiv ();
 
+      if (value !== NULL_CHARACTER
+            && this.modelDivHelper.elementOver (elem)
+            && !m.contains (value))
+      {
+        this.removeElementById (k);
+      }
+    }
 
-  resizeModel ()
-  {
-    this.modelDivHelper.fixHeight ();
+    m.each ((element) => {
+      var elem = this.findByValue (element);
+
+      // if it's in the set, ignore it ....
+      if (elem && this.modelDivHelper.elementOver (elem.getElementDiv ()))
+        return;
+
+      // if we have one but its not in the set, remove it ...
+      if (elem)
+        this.removeElementById (elem.getId ());
+
+      // add it to the set
+      this.addElement (element, {withinModel: true});
+    });
   }
 }
