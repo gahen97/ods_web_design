65bc9e64f07423f5b892d8e6d06a122aa02abcd8
seanhodges61 <seanhodges@cmail.carleton.ca>
2017-07-21 10:52:11 -0400

    Starting on BinaryTree (basics, terminology ... height, depth, size, etc)

--- public/exercise/BinaryTree/JS/Model/Traversals.js
+++ public/exercise/BinaryTree/JS/Model/Traversals.js
@@ -1,4 +1,39 @@
+/*
+  Supports:
+    - Breadth First Search (.breadthFirst)
+    - Depth First Search   (.depthFirst)
+    - In-Order Traversal   (.inorder)
+    - Pre-order Traversal  (.preorder)
+    - Post-order Traversal (.postorder)
+
+    Each calls a callback function (tree, callback)
+      with data, node
+*/
+
 class Traversal{
+  static breadthFirstSearch (rootNode, func) {
+    if (!rootNode) return false;
+
+    var nodes = [rootNode];
+    var i     = 0;
+    while (nodes.length>i){
+      var curNode = nodes [i++];
+      func (curNode.data, curNode);
+
+      if (curNode && curNode.left) nodes.push (curNode.left);
+      if (curNode && curNode.right) nodes.push (curNode.right);
+    }
+  }
+
+  static depthFirstSearch (curNode, func) {
+    if (!curNode) return false;
+
+    func (curNode.data, curNode);
+    Traversal.depthFirstSearch (curNode.left, func);
+    Traversal.depthFirstSearch (curNode.right, func);
+  }
+
+
   static inorderTraversal (curNode, func, leftPar, rightPar) {
     if (!curNode) return false;
 
@@ -40,5 +75,11 @@ class Traversal{
   }
   static postorder (tree, func){
     Traversal.postorderTraversal (tree.root, func);
+  }
+  static breadthFirst (tree, func){
+    Traversal.breadthFirstSearch (tree.root, func);
+  }
+  static depthFirst (tree, func){
+    Traversal.depthFirstSearch (tree.root, func);
   }
 }
